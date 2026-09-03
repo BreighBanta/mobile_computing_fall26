ca923d8cd21497798d9c03ab8096ea11b2921812
BreighBanta <breigh.banta@gmail.com>
2026-09-03 11:26:21 -0400

    "initial commit"

---
+++ hello_mobile/hello_mobile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hello_mobile
+//
+//  Created by Breigh Ann Banta on 9/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hello_mobile/hello_mobile/hello_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_mobileApp.swift
+//  hello_mobile
+//
+//  Created by Breigh Ann Banta on 9/1/26.
+//
+
+import SwiftUI
+
+@main
+struct hello_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
